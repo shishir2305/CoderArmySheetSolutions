b307f489c3fb1877d01e66d232c171ae38ce466a
Shishir Jha <shishir230501@gmail.com>
2023-08-19 18:48:44 +0530

    Updated

---
+++ 01_Arrays/05.cpp
@@ -0,0 +1,12 @@
+// search an element
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int element;
+    cin >> element;
+    vector<int> arr = {1, 2, 3, 4, 5};
+    return 0;
+}
